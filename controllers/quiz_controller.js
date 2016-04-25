d7618e66588b5aefe56ff72b5de53e1523b369a2
sergap1812 <sergio.hernandezs@alumnos.upm.es>
2016-04-25 16:22:39 -0400

    Tema 3: Primera Pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,13 @@
 //GET question
 exports.question = function(req, res, next) {
-	var answer = req.query.answer || '';
-	res.render('quizzes/question', {question: 'Capital de Italia', answer : answer});
+	
+	res.render('quizzes/question', {question: 'Capital de Italia'});
 };
 
 //GET check
 exports.check = function(req, res, next) {
-	var answer = req.query.answer || "";
+	
 
 	var result = req.query.answer === 'Roma' ? 'Correcta' : 'Incorrecta' ;
-	res.render('quizzes/result', {result : result, answer : answer});
+	res.render('quizzes/result', {result : result});
 };
