74968d466f8ea3f16a6f60b65995247b2fb9ff0a
sergap1812 <sergio.hernandezs@alumnos.upm.es>
2016-04-25 16:25:07 -0400

    Tema 4: No perder respuesta al reintentar

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,13 @@
 //GET question
 exports.question = function(req, res, next) {
-	
-	res.render('quizzes/question', {question: 'Capital de Italia'});
+	var answer = req.query.answer || '';
+	res.render('quizzes/question', {question: 'Capital de Italia', answer : answer});
 };
 
 //GET check
 exports.check = function(req, res, next) {
-	
+	var answer = req.query.answer || "";
 
 	var result = req.query.answer === 'Roma' ? 'Correcta' : 'Incorrecta' ;
-	res.render('quizzes/result', {result : result});
+	res.render('quizzes/result', {result : result, answer : answer});
 };
