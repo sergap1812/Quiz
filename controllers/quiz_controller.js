39cebf36bea73ab08602dccdeb109967952a3573
sergap1812 <sergio.hernandezs@alumnos.upm.es>
2016-05-01 12:31:56 -0400

    Tema 8: Bases de Datos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,35 @@
+var models = require('../models');
+
 //GET question
 exports.question = function(req, res, next) {
-	var answer = req.query.answer || '';
-	res.render('quizzes/question', {question: 'Capital de Italia', answer : answer});
+	models
+	.Quiz
+	.findOne()
+	.then(function(quiz){
+		if (quiz){
+			var answer = req.query.answer || '';
+			res
+			.render('quizzes/question', {question: quiz.question, answer: answer});
+
+		} else {
+			throw new Error('No hay preguntas en la BBDD.');
+		}
+	}).catch(function(error){ next(error);});
 };
 
 //GET check
-exports.check = function(req, res, next) {
-	var answer = req.query.answer || "";
-
-	var result = req.query.answer === 'Roma' ? 'Correcta' : 'Incorrecta' ;
-	res.render('quizzes/result', {result : result, answer : answer});
+exports.check = function(req, res, next){
+	models
+	.Quiz
+	.findOne()
+	.then(function(quiz){
+		if (quiz) {
+			var answer = res.query.answer || "";
+			var result = answer === quiz.answer ? 'Correcta' : 'Incorrecta';
+			res.render('quizzes/result', {result: result, answer: answer});
+		}
+		else {
+			throw new Error('No hat preguntas en la BBDD');
+		}
+	}).catch(function(error) { next(error); });
 };
