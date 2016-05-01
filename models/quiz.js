3171dd2d224c076be8ff51b3d9006cf23e592c42
sergap1812 <sergio.hernandezs@alumnos.upm.es>
2016-05-01 13:13:08 -0400

    Tema 9: Despliegue DB en Heroku

---
+++ models/quiz.js
@@ -0,0 +1,8 @@
+//Definición del modelo Quiz:
+module.exports = function (sequelize, DataTypes){
+	return sequelize.define('Quiz', 
+	{ question: DataTypes.STRING,
+		answer: DataTypes.STRING
+
+	});
+};
