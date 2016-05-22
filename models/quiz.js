ec59693ec8a1d98c89929fc9c1efda7fd2db2363
sergap1812 <sergio.hernandezs@alumnos.upm.es>
2016-05-22 19:55:17 -0400

    Tema14: Validación de entradas

--- models/quiz.js
+++ models/quiz.js
@@ -1,8 +1,12 @@
 //Definición del modelo Quiz:
 module.exports = function (sequelize, DataTypes){
 	return sequelize.define('Quiz', 
-	{ question: DataTypes.STRING,
-		answer: DataTypes.STRING
+	{ question: { type: DataTypes.STRING,
+						validate: { notEmpty: {msg: "Falta Pregunta"}}
+				},		
+		answer: { type: DataTypes.STRING,
+						validate: {notEmpty: {msg: "Falta Respuesta"}}
+				}
 
 	});
 };
