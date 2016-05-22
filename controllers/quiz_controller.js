bb52520bb6192b6f452279d763966b9d2d3123e7
sergap1812 <sergio.hernandezs@alumnos.upm.es>
2016-05-22 19:38:39 -0400

    Tema13: Mensajes Flash

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -96,11 +96,12 @@ exports.create = function(req, res, next){
 	var quiz = models.Quiz.build({question: req.body.quiz.question, answer: req.body.quiz.answer});
 
 //guarda en DB  los campos pregunta y respuesta del quiz
-quiz.save({fields: ["question", "answer"]})
-	.then(function(quiz){
+quiz.save({fields: ["question", "answer"]}).then(function(quiz){
+		req.flash('success', 'Quiz creado con éxito.');
 		res.redirect('/quizzes');
 	})
 	.catch(function(error){
+		req.flash('error', 'Error al crear un Quiz:' +error.messages);
 		next(error);
 	});
 
