f6b35b5702bc593bb420ea0c6a316df807acc420
sergap1812 <sergio.hernandezs@alumnos.upm.es>
2016-05-22 20:43:28 -0400

    Tema16: Borrar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -144,3 +144,15 @@ exports.update = function(req, res, next) {
 		next(error);
 	});
 }; 
+
+//DELETE
+exports.destroy = function(req, res, next){
+	req.quiz.destroy().then( function(){
+		req.flash('success', 'Quiz borrado con éxito.');
+		res.redirect('/quizzes');
+	})
+	.catch(function(error){
+		req.flash('error', 'Error al editar el Quiz: ' + error.message);
+		next(error);
+	});
+};
